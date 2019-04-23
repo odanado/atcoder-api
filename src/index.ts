9a3ceb3bde671597ecfd0229dec98685c2f276fb
odan <odan3240@gmail.com>
2019-04-24 00:08:59 +0900

    chore(lint): add @typescript-eslint

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-import axios, { AxiosInstance } from "axios"
+import axios, { AxiosInstance } from "axios";
 
-import { Response } from "./types"
+import { Response } from "./types";
 
 export class AtCoderAPI {
-    private axiosInstance: AxiosInstance;
-    constructor() {
-        this.axiosInstance = axios.create({
-            baseURL: "https://kenkoooo.com/atcoder"
-        })
-    }
+  private axiosInstance: AxiosInstance;
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: "https://kenkoooo.com/atcoder"
+    });
+  }
 
-    // TODO: add query string
-    private async callGet<T>(path: string): Promise<T> {
-        const res = await this.axiosInstance.get<T>(path)
-        return res.data
-    }
+  // TODO: add query string
+  private async callGet<T>(path: string): Promise<T> {
+    const res = await this.axiosInstance.get<T>(path);
+    return res.data;
+  }
 
-    public contests(): Promise<Response.Contests> {
-        return this.callGet('/resources/contests.json')
-    }
-    public problems(): Promise<Response.Problems> {
-        return this.callGet('/resources/problems.json')
-    }
-    public mergedProblems(): Promise<Response.MergedProblems> {
-        return this.callGet('/resources/merged-problems.json')
-    }
+  public contests(): Promise<Response.Contests> {
+    return this.callGet("/resources/contests.json");
+  }
+  public problems(): Promise<Response.Problems> {
+    return this.callGet("/resources/problems.json");
+  }
+  public mergedProblems(): Promise<Response.MergedProblems> {
+    return this.callGet("/resources/merged-problems.json");
+  }
 }
 
 async function main() {
-    const client = new AtCoderAPI()
-    console.log((await client.mergedProblems())[0])
+  // const client = new AtCoderAPI()
+  // console.log((await client.mergedProblems())[0])
 }
 
-main()
+main();
