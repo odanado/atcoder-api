6924cad2db753db665aa0b48fa57d25098bc70c8
odan <odan3240@gmail.com>
2019-04-23 23:55:05 +0900

    feat(api): implement information api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
-const a = 10
+import axios, { AxiosInstance } from "axios"
 
-function poyo() {
-    return 1 + 2;
+import { Response } from "./types"
+
+export class AtCoderAPI {
+    private axiosInstance: AxiosInstance;
+    constructor() {
+        this.axiosInstance = axios.create({
+            baseURL: "https://kenkoooo.com/atcoder"
+        })
+    }
+
+    // TODO: add query string
+    private async callGet<T>(path: string): Promise<T> {
+        const res = await this.axiosInstance.get<T>(path)
+        return res.data
+    }
+
+    public contests(): Promise<Response.Contests> {
+        return this.callGet('/resources/contests.json')
+    }
+    public problems(): Promise<Response.Problems> {
+        return this.callGet('/resources/problems.json')
+    }
+    public mergedProblems(): Promise<Response.MergedProblems> {
+        return this.callGet('/resources/merged-problems.json')
+    }
 }
 
-export { poyo, a };
+async function main() {
+    const client = new AtCoderAPI()
+    console.log((await client.mergedProblems())[0])
+}
+
+main()
---
+++ src/types.d.ts
@@ -0,0 +1,42 @@
+export declare namespace Response {
+    export interface Contest {
+        id: string;
+        start_epoch_second: number;
+        duration_second: number;
+        title: string;
+        rate_change: string;
+    }
+    export type Contests = Contest[]
+
+    export interface Problem {
+        id: string;
+        contest_id: string;
+        title: string;
+    }
+    export type Problems = Problem[]
+
+    export interface MergedProblem {
+        id: string,
+        contest_id: string,
+        title: string,
+        shortest_submission_id: number,
+        shortest_problem_id: string,
+        shortest_contest_id: string,
+        shortest_user_id: string,
+        fastest_submission_id: number,
+        fastest_problem_id: string,
+        fastest_contest_id: string,
+        fastest_user_id: string,
+        first_submission_id: number,
+        first_problem_id: string,
+        first_contest_id: string,
+        first_user_id: string,
+        source_code_length: number,
+        execution_time: number,
+        point: number,
+        predict?: number,
+        solver_count: number
+    }
+    export type MergedProblems = MergedProblem[]
+
+}
